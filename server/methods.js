56ab13531e9aaf11d792f8eae8a0d947e6f1bda4
Loan Laux <contact@loanlaux.fr>
2018-05-05 19:38:46 +0200

    Fix settings fetching in method

--- server/methods.js
+++ server/methods.js
@@ -1,10 +1,10 @@
 import { Meteor } from "meteor/meteor";
 import { check } from "meteor/check";
 import { Reaction } from "/server/api";
+import { ReactionProduct } from "/lib/api";
 import { Orders, Packages } from "/lib/collections";
 import { ProductReviews } from "../lib/collection";
 import hasUserPurchasedProduct from "../lib/hasUserPurchasedProduct";
-import { ReactionProduct } from "/lib/api";
 
 Meteor.methods({
   "loanlaux/submitProductReview": (rating, productId) => {
@@ -12,19 +12,19 @@ Meteor.methods({
     check(productId, String);
 
     const userId = Meteor.userId();
-    const packageSettings = Reaction.getPackageSettings("loanlaux-product-reviews");
+    const { settings } = Reaction.getPackageSettings("loanlaux-product-reviews");
     const userOrders = Orders.find({ userId }).fetch();
     const currentProductId = ReactionProduct.selectedProductId();
 
     userHasPurchasedProduct = hasUserPurchasedProduct(userOrders, currentProductId);
 
-    if (!packageSettings.public.allowReviewsFromGuests &&
+    if (!settings.public.allowReviewsFromGuests &&
       !Reaction.hasPermission("account/profile", userId, Reaction.getShopId())) {
 
       throw new Meteor.Error("access-denied", "You need to be a member to review this product");
     }
 
-    if (!packageSettings.public.allowReviewsWithoutPurchasing && !userHasPurchasedProduct) {
+    if (!settings.public.allowReviewsWithoutPurchasing && !userHasPurchasedProduct) {
 
       throw new Meteor.Error("access-denied", "You need to purchase this product to review it");
     }
